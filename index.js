fdef39e3856dc52614cf5f355a5e9b7ad5a1e86a
vidya-bingi-26 <vidyabingi26@gmail.com>
2025-01-26 11:11:02 +0530

    add githubdata

--- index.js
+++ index.js
@@ -12,8 +12,32 @@ app.get('/instagram',(req,res)=>{
     res.send("vidya__02")
 })
 
+const data=[{
+  "login": "mojombo",
+  "id": 1,
+  "node_id": "MDQ6VXNlcjE=",
+  "avatar_url": "https://avatars.githubusercontent.com/u/1?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/mojombo",
+  "html_url": "https://github.com/mojombo",
+  "followers_url": "https://api.github.com/users/mojombo/followers",
+  "following_url": "https://api.github.com/users/mojombo/following{/other_user}",
+  "gists_url": "https://api.github.com/users/mojombo/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/mojombo/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/mojombo/subscriptions",
+  "organizations_url": "https://api.github.com/users/mojombo/orgs",
+  "repos_url": "https://api.github.com/users/mojombo/repos",
+  "events_url": "https://api.github.com/users/mojombo/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/mojombo/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false
+}]
 
 
+app.get('/github',(req,res)=>{
+  res.send(data);
+})
 app.listen(port,() => {
     console.log(`server running on ${port}`)
 })
